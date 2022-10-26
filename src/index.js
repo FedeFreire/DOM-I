de8e87d5f872f1425017274d745181f3d3adb888
FedeFreire <bandoleir@gmail.com>
2022-10-26 11:29:58 -0400

    100% Done

--- src/index.js
+++ src/index.js
@@ -40,3 +40,102 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+
+//------------------- NAV BAR //
+
+const navBar = document.querySelectorAll('nav a');
+navBar[0].textContent = "Services";
+navBar[1].textContent = "Product";
+navBar[2].textContent = "Vision";
+navBar[3].textContent = "Features";
+navBar[4].textContent = "About";
+navBar[5].textContent = "Contact";
+
+siteContent.nav["nav-item-1"] = navBar[0];
+siteContent.nav["nav-item-2"] = navBar[1];
+siteContent.nav["nav-item-3"] = navBar[2];
+siteContent.nav["nav-item-4"] = navBar[3];
+siteContent.nav["nav-item-5"] = navBar[4];
+siteContent.nav["nav-item-6"] = navBar[5];
+
+navBar[0].classList.add("italic")
+navBar[1].classList.add("italic")
+navBar[2].classList.add("italic")
+navBar[3].classList.add("italic")
+navBar[4].classList.add("italic")
+navBar[5].classList.add("italic")
+
+// IMGS
+
+const logo = document.querySelector('#logo-img');
+logo.setAttribute('src', "http://localhost:9000/img/logo.png")
+
+// CTA
+
+const cta = document.querySelector('#cta-img');
+cta.setAttribute('src', "http://localhost:9000/img/cta.png")
+
+let ctaTextH1 = document.querySelectorAll('.cta-text h1');
+ctaTextH1[0].textContent = "DOM Is Awesome"
+let ctaTextButton = document.querySelectorAll('.cta-text button');
+ctaTextButton[0].textContent = "Get Started"
+
+let mainContentH4 = document.querySelectorAll(".main-content h4");
+
+mainContentH4[0].textContent = "Features";
+mainContentH4[1].textContent = "About";
+mainContentH4[2].textContent = "Services";
+mainContentH4[3].textContent = "Product";
+mainContentH4[4].textContent = "Vision";
+
+let midImg = document.querySelector('#middle-img');
+midImg.src = 'http://localhost:9000/img/accent.png'
+
+
+let mainContentP = document.querySelectorAll(".main-content P");
+
+mainContentP[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let contact = document.querySelector('.contact').children;
+contact[0].textContent = 'Contact';
+contact[1].textContent = '123 Way 456 Street Somewhere, USA';
+contact[2].textContent = '1 (888) 888-8888';
+contact[3].textContent = 'sales@greatidea.io';
+
+let footer = document.querySelector('footer').children;
+
+footer[0].classList.add('bold')
+footer[0].textContent = 'Copyright Great Idea! 2021'
+
+
+// const featuresH4 = document.querySelector('.text-content h4');
+// featuresH4.textContent = "Features";
+// const featuresP = document.querySelector('.text-content p');
+// featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// const featuresH4 = document.querySelector('.text-content h4');
+// featuresH4.textContent = "Features";
+// const featuresP = document.querySelector('.text-content p');
+// featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// const featuresH4 = document.querySelector('.text-content h4');
+// featuresH4.textContent = "Features";
+// const featuresP = document.querySelector('.text-content p');
+// featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// const featuresH4 = document.querySelector('.text-content h4');
+// featuresH4.textContent = "Features";
+// const featuresP = document.querySelector('.text-content p');
+// featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// const featuresH4 = document.querySelector('.text-content h4');
+// featuresH4.textContent = "Features";
+// const featuresP = document.querySelector('.text-content p');
+// featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
